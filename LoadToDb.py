0695386b08e92f80dae300f0c3f8c08246ca1263
Kimi-Codes <vagrant@vagrant.vm>
2020-11-10 19:27:32 +0200

    fixed: 'get_price_types' function bug of mislabeling price type. added: update_price_history_query, update_current_price_query, update_product_query.

--- LoadToDb.py
+++ LoadToDb.py
@@ -1,11 +1,13 @@
-import json
-import xmltodict
-import mysql.connector
-from mysql.connector import errorcode
 from datetime import date, timedelta, datetime
+from mysql.connector import errorcode
+from collections import namedtuple
+import mysql.connector
+import xmltodict
 import requests
+import json
 import re
 
+PRICE, FULLPRICE, PROMO, FULLPROMO = 'Price', 'PriceFull', 'Promo', 'PromoFull' 
 
 # Creates a connection to the db. Returns connector or None if failed to connect
 def connect_to_db():
@@ -44,6 +46,50 @@ def exec_queries(query_list):
     return 1
 
 
+# creates a query for adding a new price log to PriceHistory table
+def update_price_history_query(chain_id, branch_id, date, product):
+    table_name = "PriceHistory"
+    price_history_keys = "(pid, cid, bid, price, update_date)"
+    values = str((product['ItemCode'], chain_id, branch_id, product['ItemPrice'], date))
+
+    # gives 1 if last added price is equal to the new price, 0 if not.
+    last_price_equal = f"SELECT COUNT(*) FROM {table_name} " \
+                       f"WHERE pid = {product['ItemCode']} " \
+                       f"AND cid = {str(chain_id)} " \
+                       f"AND bid = {str(branch_id)} " \
+                       f"AND price = {product['ItemPrice']} " \
+                       f"AND date <= {date} " \
+                       f"ORDER BY date DESC " \
+                       f"LIMIT 1"
+    # query for adding new log to the table if price has changed
+    query = f"INSERT INTO {table_name} {price_history_keys} " \
+            f"SELECT {values} " \
+            f"WHERE ({last_price_equal}) = 0; "
+    return query
+
+
+def update_current_price_query(chain_id, branch_id, date, product):
+    table_name = 'CurrentPrices'
+    curr_price_keys = "(pid, cid, bid, price, update_date)"
+    values = str((product['ItemCode'], chain_id, branch_id, product['ItemPrice'], date))
+
+    query = f"REPLACE INTO {table_name} {curr_price_keys} " \
+            f"VALUES {values}; "
+    return query
+    
+
+def update_product_query(chain_id, branch_id, date, product):
+    table_name = 'Products'
+    products_keys = "(pid, cid, bid, pname, manufacturer)"
+    values = str((product['ItemCode'], chain_id, branch_id, product['ItemName'], product['ManufactureName']))
+
+    query = f"REPLACE INTO {table_name} {products_keys} " \
+            f"VALUES {values}; "
+    return query
+
+
+# ==============
+
 # scrapes file download paths, returns a list containing the paths
 def get_file_paths(code, date = '', file_type = 'all'):
     host = 'http://matrixcatalog.co.il'
@@ -65,22 +111,41 @@ def get_file_paths(code, date = '', file_type = 'all'):
     return file_paths
 
 
-# Returns a list ot tuples structured as (filetype, path).
-def get_price_types(file_paths, price_type=['all'], all_price_types=[]): #load_full_price=False, start_from_full_price_file=False,
-    ALL_PRICE_TYPES_DEF = ['Price', 'PriceFull', 'Promo', 'PromoFull']
-    all_price_types = ALL_PRICE_TYPES_DEF if not all_price_types else all_price_types
-    is_all_type = 'all' in price_type
+# Returns a list of namedtuples structured as PriceFile('type','path') 
+def get_price_types(file_paths, select_price_types=['all'], all_price_types=[PRICE, FULLPRICE, PROMO, FULLPROMO]):
+    # reverse sort makes sub strings (starting at index 0) to be after the full string
+    # to make sure a match is made to the longest string possible.
+    # for example: 'PriceFull' can be mistakenly labeled as 'Price'
+    all_price_types = list(all_price_types)
+    all_price_types.sort(reverse=True)
+    select_price_types = list(select_price_types)
+    select_price_types.sort(reverse=True)
+    if 'all' in select_price_types:
+        select_price_types = all_price_types
+
     filename_regex = r'/([^/]*)(/)?$'
-    if is_all_type:
-        price_type = all_price_types
-    filetype_regex = "({})".format('|'.join(price_type))
+    pricetype_regex = "^({})".format('|'.join(all_price_types))
     
-    paths=[]
+    PriceFile = namedtuple('PriceFile', ['type', 'path'])
+    pricefiles=[]
     for path in file_paths:
         filename = re.compile(filename_regex).search(path)
-        filetype = re.compile(filetype_regex).search(filename[1])
-        if not filetype:
+        filetype = re.compile(pricetype_regex).search(filename[1])
+        if not filetype or filetype[1] not in select_price_types:
             continue
-        paths.append((filetype[1], path))
+        pricefiles.append(PriceFile(filetype[1], path))
     
-    return paths
+    return pricefiles
+
+
+def foo(price_files):
+    for f in price_files:
+        if f.type == FULLPRICE:
+            print(f)
+        elif f.type == PROMO:
+            pass
+
+
+
+    
+    
